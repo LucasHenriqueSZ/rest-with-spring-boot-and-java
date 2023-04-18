467d701dab0493f8f46eb86af1b53ec1a754c179
Lucas Henrique <96556583+LucasHenriqueSZ@users.noreply.github.com>
2023-04-18 01:08:20 -0300

    calculator implementation and exception handling

---
+++ api/src/main/java/com/rest/api/math/SimpleMath.java
@@ -0,0 +1,27 @@
+package com.rest.api.math;
+
+public class SimpleMath {
+    public double sum(double a, double b) {
+        return a + b;
+    }
+
+    public double subtraction(double a, double b) {
+        return a - b;
+    }
+
+    public double multiplication(double a, double b) {
+        return a * b;
+    }
+
+    public double division(double a, double b) {
+        return a / b;
+    }
+
+    public double average(double a, double b) {
+        return (a + b) / 2;
+    }
+
+    public double squareRoot(double a) {
+        return Math.sqrt(a);
+    }
+}
---
+++ api/src/main/java/com/rest/api/util/NumberConverter.java
@@ -0,0 +1,19 @@
+package com.rest.api.util;
+
+public class NumberConverter {
+    public static double convertToDouble(String numberStr) {
+        if (numberStr == null)
+            return 0D;
+        String number = numberStr.replaceAll(",", ".");
+        if (isNumeric(number))
+            return Double.parseDouble(number);
+        return 0D;
+    }
+
+    public static boolean isNumeric(String numberStr) {
+        if (numberStr == null)
+            return false;
+        String number = numberStr.replaceAll(",", ".");
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
+}
